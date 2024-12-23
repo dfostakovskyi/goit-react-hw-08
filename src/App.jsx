d860e7adca68e9d5c246ebd58fdaa2934d886747
dfostakovskyi <d.fostakovskiy@gmail.com>
2024-12-22 22:00:08 -0800

    6

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import ContactList from "./components/contactList/contactList";
+import ContactList from "./components/contactList/contactsList";
 import SearchBox from "./components/searchBox/SearchBox";
 import ContactForm from "./components/contactForm/ContactForm";
 import { nanoid } from "nanoid";
